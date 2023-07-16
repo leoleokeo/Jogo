407f6e15f87503aeab3a2d0fe22feccec0a3943d
leoleokeo <leonardogachet10@gmail.com>
2023-07-16 16:06:53 -0300

    melhorado menu de pause

--- Jogo Backup/Scripts/pause_menu.gd
+++ Jogo Backup/Scripts/pause_menu.gd
@@ -1,15 +1,13 @@
-extends Control
+extends CanvasLayer
 
 var isPaused = false
 
 func _ready():
 	hide()
 	get_tree().paused = false
-
 func _input(event):
 	if event.is_action_pressed("pause"):
 		toggle_pause()
-
 func toggle_pause():
 	isPaused = !isPaused
 	if isPaused:
@@ -18,3 +16,11 @@ func toggle_pause():
 	else:
 		hide()
 		get_tree().paused = false
+func _on_Button_pressed():
+	get_tree().quit()
+
+func _on_pause_quit_btn_pressed():
+	get_tree().quit()
+
+func _on_pause_continue_btn_pressed():
+	toggle_pause()
