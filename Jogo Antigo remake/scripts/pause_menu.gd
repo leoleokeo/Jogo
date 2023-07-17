3ddcf15ddf9476e5ad886f079f16e80a4aa4945a
leoleokeo <leonardogachet10@gmail.com>
2023-07-17 10:11:36 -0300

    adicionado menu de pause

---
+++ Jogo Antigo remake/scripts/pause_menu.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+var show = false
+
+func _ready():
+	hide()
+	get_tree().paused = false
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+func toggle_pause():
+	show = !show
+	
+	if show:
+		get_tree().paused = true
+		show()
+		Global.salvar()
+	elif !show:
+		get_tree().paused = false
+		hide()
+
+
+func _on_pause_quit_btn_pressed():
+	get_tree().quit()
+	Global.salvar()
+func _on_back_menu_pause_btn_pressed():
+	toggle_pause()
+	get_tree().change_scene("res://prefabs/start_screen.tscn")
+func _on_continuar_pause_btn_pressed():
+	toggle_pause()
